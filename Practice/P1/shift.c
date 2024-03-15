b6a65afa0535bd4a15bcd98a86240fb1387baf65
Vitaly <vitalydude83@gmail.com>
2024-03-15 17:04:21 +0300

    array_shift

--- Practice/P1/shift.c
+++ Practice/P1/shift.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 void print(int*, int);
+void shift (int* arr, int length, int number);
+void one_element_shift (int* arr, int length);
 int main (){
     int a = 0;
     scanf("%d", &a); // ввод пользователя
@@ -12,7 +14,13 @@ int main (){
     arr = (int*)realloc(arr, sizeof(int) * a);
     if(arr == NULL) return 1;
     print(arr, a);
-   
+    for(int i = 0; i < a; ++i)
+        arr[i] = i;
+    print(arr, a);
+    int shift_num = 0;
+    scanf("%d", &shift_num);
+    shift (arr, a, shift_num);
+    print(arr, a);
     free(arr);
 return 0;
 }
@@ -21,4 +29,16 @@ void print(int* arr, int a){
         printf("%d ", arr[i]);
     printf("\n");
 
+}
+void one_element_shift (int* arr, int length){
+    int a = 0;
+    a = arr[length - 1];
+    for(int i = 1; i < length; ++i){
+        arr[length - i] = arr[length - i - 1];
+    }
+    arr[0] = a;
+}
+void shift (int* arr, int length, int number){
+    for(int i = 0; i < number; ++i)
+        one_element_shift (arr, length);
 }
