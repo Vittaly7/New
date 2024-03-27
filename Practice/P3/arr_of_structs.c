04766fd1029ee6502a854ef6c0f7d568592d1dd4
Vitaly <vitalydude83@gmail.com>
2024-03-27 15:50:35 +0300

    new commit

--- Practice/P3/arr_of_structs.c
+++ Practice/P3/arr_of_structs.c
@@ -12,12 +12,13 @@ int main (){
     printf("%d, %d\n", p1.x, p1.y);
 //storing on heap
     Point* p2;
-    p2 = (int*)malloc(sizeof(Point));
-    if(p2 == NULL); return 1;
+    p2 = malloc(sizeof(Point));
+// почему срабатывает условие if, если убрать комментарии?
+    //if(p2 == NULL); return 1;
     (*p2).x = 10;
     (*p2).y = 2;
     printf("%d, %d\n", (*p2).x, (*p2).y);
-
+    free (p2);
 
     return 0;
 }
