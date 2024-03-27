066bcaef89840d9d56e5d5b598ebdb83d31e7b1d
Vitaly <vitalydude83@gmail.com>
2024-03-27 15:43:29 +0300

    new commit

--- Practice/P3/arr_of_structs.c
+++ Practice/P3/arr_of_structs.c
@@ -7,9 +7,16 @@ typedef struct{
 } Point;
 
 int main (){
+//storing on stack
     Point p1 = {.x = 4, .y = 5};
     printf("%d, %d\n", p1.x, p1.y);
-
+//storing on heap
+    Point* p2;
+    p2 = (int*)malloc(sizeof(Point));
+    if(p2 == NULL); return 1;
+    (*p2).x = 10;
+    (*p2).y = 2;
+    printf("%d, %d\n", (*p2).x, (*p2).y);
 
 
     return 0;
