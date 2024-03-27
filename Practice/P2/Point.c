29b5ab4cdf09e2a071cd51cfd3c4db65a50e2b8e
Vitaly <vitalydude83@gmail.com>
2024-03-27 12:06:30 +0300

    сщздание точки

---
+++ Practice/P2/Point.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct{
+    int x;
+    int y;
+} P;
+
+P coordinates (int a, int b);
+
+int main(){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    P point = coordinates(a, b);
+    printf("Point: %d : %d\n", point.x, point.y);
+
+return 0;
+}
+
+P coordinates (int a, int b){
+    P point;
+    if(a, b >= 0){
+        point.x = a;
+        point.y = b;
+        return point;
+    }
+    else printf("invalid coordinates :(\n");
+}
