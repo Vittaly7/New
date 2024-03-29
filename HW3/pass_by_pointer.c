4bbc0ed8744ea909f0941c36ac8794ece9d25b23
Vitaly <vitalydude83@gmail.com>
2024-03-29 16:22:39 +0300

    first commit

---
+++ HW3/pass_by_pointer.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct{
+    int x;
+    int y;
+} P;
+
+P coordinates (int a, int b);
+
+int main(){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    P point = coordinates(a, b);
+    printf("Point: %d : %d\n", point.x, point.y);
+
+return 0;
+}
+
+P coordinates (int a, int b){
+    P* point;
+    point = (P*)malloc(sizeof(point));
+    if(a >= 0 && b >= 0){
+        (*point).x = a;
+        (*point).y = b;
+        return (*point);
+    }
+    else printf("invalid coordinates :(\n");
+}
