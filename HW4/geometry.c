0f13c0e5f1343e8c69ab57bf742670dfd1c670a1
Vitaly <vitalydude83@gmail.com>
2024-03-30 08:18:09 +0300

    все сломал

--- HW4/geometry.c
+++ HW4/geometry.c
@@ -1,39 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
-char field [30][60];
+const int width = 30;
+const int hight = 80;
+char field [width][hight];
 // 1. поле 30 : 60
 // 2. считать имя фигуры
 // 3. считать координаты вершин
 // 4. валидация
 // 5. отрисовка фигур
 
-void grid ();
-void grid_output();
+void grid (int width, int hight);
+void grid_output(char field[][]);
 
 
 int main (){
 
-    grid ();
-    grid_output();
+    grid (width, hight);
+    grid_output(field[width][hight]);
 
     return 0;
 }
 // создаем матрицу и заполняем ее.
-void grid (){
-    char field [30][60];
-    for(int i = 0; i < 30; ++i){
-        for(int j = 0; j < 60; ++j){
-            if(i == 0 || j == 0 || i == 29 || j == 59)
-                field[i][j] = "#";
+void grid (int width, int hight){
+    char field [width][hight];
+    for(int i = 0; i < width; ++i){
+        for(int j = 0; j < hight; ++j){
+            if(i == 0 || j == 0 || i == width - 1 || j == hight - 1)
+                field[i][j] = '#';
             else
-                field[i][j] = " ";
+                field[i][j] = ' ';
         }
     }
-}
-
-void grid_output(){
-    for(int i = 0; i < 30; ++i){
-        for(int j = 0; j < 60; ++j){
+    }
+// выводим на экран
+void grid_output(char field[width][hight]){
+    for(int i = 0; i < width; ++i){
+        for(int j = 0; j < hight; ++j){
             printf("%c", field[i][j]);
         }
         printf("\n");
