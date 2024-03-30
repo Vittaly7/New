e0401ce412bdb4ce2a7cbff7adae3bec93a7629c
Vitaly <vitalydude83@gmail.com>
2024-03-30 20:17:59 +0300

    draft. nothing works :)

--- HW4/geometry.c
+++ HW4/geometry.c
@@ -5,12 +5,17 @@
 #define hight 80;
 
 char field [width][hight];
+//make a new type for a point
+typedef struct{
+    int x;
+    int y;
+} point;
 // 1. поле 30 : 80
 // 2. считать имя фигуры
 // 3. считать координаты вершин
 // 4. валидация
 // 5. отрисовка фигур
-
+void coordinates(point* triangle, point* square, point* rectangle);
 void grid (width, hight);
 void grid_output(char field[][]);
 
@@ -19,6 +24,21 @@ int main (){
 
     grid (width, hight);
     grid_output(field[width][hight]);
+// allocate memory for shapes
+// allocate memory for a triangle
+    point* triangle;
+    triangle == (point*)mallock(sizeof(point) * 3);
+    if(triangle == NULL) return 1;
+// allocate memory for a square
+    point* square;
+    square == (point*)mallock(sizeof(point) * 4);
+    if(square == NULL) return 1;
+// allocate memory for a rectangle
+    point* rectangle;
+    rectangle == (point*)mallock(sizeof(point) * 3);
+    if(rectangle == NULL) return 1;
+
+
 
     return 0;
 }
@@ -41,6 +61,39 @@ void grid_output(char field[width][hight]){
             printf("%c", field[i][j]);
         }
         printf("\n");
+    }
+
+}
+// read coordinates
+void coordinates(point* triangle, point* square, point* rectangle){
+    int a = 0;
+    printf("Pick a number\n");
+    printf("1 - a triangle\n");
+    printf("2 - a square");
+    printf("3 - a rectangle");
+
+    scanf("%d", &a);
+    if(a < 1 || a > 3){
+        printf("Pick a NEW number\n");
+        printf("1 - a triangle\n");
+        printf("2 - a square");
+        printf("3 - a rectangle");
+
+        scanf("%d", &a);
+    }
+// read coordinates for a triangle
+    else if(a == 1){
+        for(int i = 0; i < 3; ++i){
+            printf("Type coordinates: ");
+            scanf("%d %d", &triangle[i].x, &triangle[i].y);
+        }
+    }
+// // read coordinates for a square
+    else if(a == 2){
+        for(int i = 0; i < 4; ++i){
+            printf("Type coordinates: ");
+            scanf("%d %d", &square[i].x, &square[i].y);
+        }
     }
 
 }
