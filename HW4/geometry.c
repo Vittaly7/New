b15c195b06ce12d06d9c05ed677dac1a7982c0fd
Vitaly <vitalydude83@gmail.com>
2024-04-10 11:20:51 +0300

    new commit

--- HW4/geometry.c
+++ HW4/geometry.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define width 30
-#define hight 80
+#define hight 30
+#define width 80
 
-char field [width][hight];
+char field [hight][width];
 //make a new type for a point
 typedef struct{
     int x;
@@ -14,42 +14,32 @@ typedef struct{
 // 2. считать имя фигуры
 // 3. считать координаты вершин
 // 4. валидация
+int check (point*);
 // 5. отрисовка фигур
-void coordinates(point* triangle, point* square, point* rectangle);
-void grid (int width, int hight);
-void grid_output(char field[width][hight]);
+point* coordinates();
+void grid ();
+void grid_output(char field[hight][width]);
 
 
 int main (){
     
-    grid (width, hight);
+    grid ();
     grid_output(field);
-// allocate memory for shapes
-// allocate memory for a triangle
-    point* triangle;
-    triangle = (point*)malloc(sizeof(point) * 3);
-    if(triangle == NULL) return 1;
-// allocate memory for a square
-    point* square;
-    square = (point*)malloc(sizeof(point) * 4);
-    if(square == NULL) return 1;
-// allocate memory for a rectangle
-    point* rectangle;
-    rectangle = (point*)malloc(sizeof(point) * 3);
-    if(rectangle == NULL) return 1;
+    point* catch = coordinates();
+// печатаем координаты
+    int b = check(catch);
+    printf("%d\n", b);
 
-
-    free(triangle);
-    free(square);
-    free(rectangle);
-
+        
+    free(catch);
+    
     return 0;
 }
 // создаем матрицу и заполняем ее.
-void grid (int width, int hight){    
-    for(int i = 0; i < width; ++i){
-        for(int j = 0; j < hight; ++j){
-            if(i == 0 || j == 0 || i == width - 1 || j == hight - 1)
+void grid (){
+    for(int i = 0; i < hight; ++i){
+        for(int j = 0; j < width; ++j){
+            if(i == 0 || j == 0 || i == hight - 1 || j == width - 1)
                 field[i][j] = '#';
             else
                 field[i][j] = ' ';
@@ -57,9 +47,9 @@ void grid (int width, int hight){
     }
     }
 // выводим на экран
-void grid_output(char field[width][hight]){
-    for(int i = 0; i < width; ++i){
-        for(int j = 0; j < hight; ++j){
+void grid_output(char field[hight][width]){
+    for(int i = 0; i < hight; ++i){
+        for(int j = 0; j < width; ++j){
             printf("%c", field[i][j]);
         }
         printf("\n");
@@ -67,35 +57,53 @@ void grid_output(char field[width][hight]){
 
 }
 // read coordinates
-void coordinates(point* triangle, point* square, point* rectangle){
+point* coordinates(){
     int a = 0;
-    printf("Pick a number\n");
-    printf("1 - a triangle\n");
-    printf("2 - a square");
-    printf("3 - a rectangle");
-
-    scanf("%d", &a);
-    if(a < 1 || a > 3){
+    while(a < 1 || a > 3){
         printf("Pick a NEW number\n");
         printf("1 - a triangle\n");
-        printf("2 - a square");
-        printf("3 - a rectangle");
+        printf("2 - a square\n");
+        printf("3 - a rectangle\n");
 
         scanf("%d", &a);
     }
 // read coordinates for a triangle
-    else if(a == 1){
+    if(a == 1){
+        point* triangle;
+        triangle = (point*)malloc(sizeof(point) * 3);
+        if(triangle == NULL) return 1;
         for(int i = 0; i < 3; ++i){
             printf("Type coordinates: ");
             scanf("%d %d", &triangle[i].x, &triangle[i].y);
         }
+        return triangle;
     }
 // // read coordinates for a square
     else if(a == 2){
+        point* square;
+        square = (point*)malloc(sizeof(point) * 4);
+        if(square == NULL) return 1;
         for(int i = 0; i < 4; ++i){
             printf("Type coordinates: ");
             scanf("%d %d", &square[i].x, &square[i].y);
         }
+        return square;
+    }
+    else if(a == 3){
+        point* rectangle;
+        rectangle = (point*)malloc(sizeof(point) * 4);
+        if(rectangle == NULL) return 1;
+        for(int i = 0; i < 4; ++i){
+            printf("Type coordinates: ");
+            scanf("%d %d", &rectangle[i].x, &rectangle[i].y);
+        }
+        return rectangle;
     }
 
 }
+int check (point*a){
+        if((width - a[0].x) / (a[1].x - a[0].x) == (hight - a[0].y) / (a[1].y - a[0].y))
+            return 0;
+        else
+            return 1;
+    }
