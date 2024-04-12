abae77da5ebf0deb0066cdc6918d8c46efbe6517
Vitaly <vitalydude83@gmail.com>
2024-04-12 20:09:45 +0300

    apexes

--- HW4/geometry.c
+++ HW4/geometry.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int number_of_apexes = 0;
 #define hight 30
 #define width 80
 
@@ -11,27 +12,31 @@ typedef struct{
     int y;
 } point;
 // 1. поле 30 : 80
+void grid ();
 // 2. считать имя фигуры
 // 3. считать координаты вершин
+point* coordinates();
 // 4. валидация
 int check (point*);
-// 5. отрисовка фигур
-point* coordinates();
-void grid ();
+// 5. расставить точки
+void apex_output(point* array_of_apexes);
+// 6. отрисовка фигур
 void grid_output(char field[hight][width]);
 
 
 int main (){
     
     grid ();
+    point* array_of_apexes = coordinates();
+    if(array_of_apexes == NULL) return 1;
+    int is_valid = check(array_of_apexes);
+    printf("%d\n", is_valid);
+    apex_output(array_of_apexes);
     grid_output(field);
-    point* catch = coordinates();
 // печатаем координаты
-    int b = check(catch);
-    printf("%d\n", b);
 
         
-    free(catch);
+    free(array_of_apexes);
     
     return 0;
 }
@@ -69,9 +74,10 @@ point* coordinates(){
     }
 // read coordinates for a triangle
     if(a == 1){
+        number_of_apexes = 3;
         point* triangle;
         triangle = (point*)malloc(sizeof(point) * 3);
-        if(triangle == NULL) return 1;
+        if(triangle == NULL) return NULL;
         for(int i = 0; i < 3; ++i){
             printf("Type coordinates: ");
             scanf("%d %d", &triangle[i].x, &triangle[i].y);
@@ -80,9 +86,10 @@ point* coordinates(){
     }
 // // read coordinates for a square
     else if(a == 2){
+        number_of_apexes = 4;
         point* square;
         square = (point*)malloc(sizeof(point) * 4);
-        if(square == NULL) return 1;
+        if(square == NULL) return NULL;
         for(int i = 0; i < 4; ++i){
             printf("Type coordinates: ");
             scanf("%d %d", &square[i].x, &square[i].y);
@@ -90,9 +97,10 @@ point* coordinates(){
         return square;
     }
     else if(a == 3){
+        number_of_apexes = 4;
         point* rectangle;
         rectangle = (point*)malloc(sizeof(point) * 4);
-        if(rectangle == NULL) return 1;
+        if(rectangle == NULL) return NULL;
         for(int i = 0; i < 4; ++i){
             printf("Type coordinates: ");
             scanf("%d %d", &rectangle[i].x, &rectangle[i].y);
@@ -102,8 +110,15 @@ point* coordinates(){
 
 }
 int check (point*a){
-        if((width - a[0].x) / (a[1].x - a[0].x) == (hight - a[0].y) / (a[1].y - a[0].y))
-            return 0;
-        else
-            return 1;
+    if(a[1].x == a[0].x || a[1].y == a[0].y)
+        return 0;
+    else if((width - a[0].x) / (a[1].x - a[0].x) == (hight - a[0].y) / (a[1].y - a[0].y)) // вероятно добавить еще оду проверку
+        return 0;
+    else
+        return 1;
+}
+void apex_output(point* array_of_apexes){
+    for(int i = 0; i < number_of_apexes; ++i){
+        field[array_of_apexes[i].x][array_of_apexes[i].y] = '#';
     }
+}
